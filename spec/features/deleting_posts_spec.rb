841e3d78f52f0f4303a62700ac466d2cdecc98f5
Nalipp <nateplipp@gmail.com>
2016-01-10 17:52:02 +0900

    add delete post restrictions

--- spec/features/deleting_posts_spec.rb
+++ spec/features/deleting_posts_spec.rb
@@ -4,17 +4,25 @@ feature 'Deleting posts' do
 
   background do
     user = create :user
+    user_two = create(:user, email: 'example@gmail.com',
+                          user_name: 'thepassword',
+                          id: user.id + 1)
     post = create(:post, caption: 'Abs for days.', user_id: user.id)
+    post_two = create(:post, user_id: user.id + 1)
 
     sign_in_with user
-
-    find(:xpath, "//a[contains(@href,'posts/1')]").click
   end
 
-  scenario 'Can delete a single post' do
+  scenario 'Can delete a single post as the owner' do
+    find(:xpath, "//a[contains(@href,'posts/1')]").click
     click_link 'Delete Post'
 
     expect(page).to have_content('Problem solved!  Post deleted.')
     expect(page).to_not have_content('Abs for days.')
+  end
+
+  scenario 'Can not delete a post that does not belong to use via the show page' do
+    find(:xpath, "//a[contains(@href,'posts/2')]").click
+    expect(page).to_not have_content('Delete Post')
   end
 end
