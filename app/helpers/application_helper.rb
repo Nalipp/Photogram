e86a0218bddd272fd60e29651614b26f9682d614
Nalipp <nateplipp@gmail.com>
2016-01-12 16:47:52 +0900

    add image preview to form posts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-=begin
   def alert_for(flash_type)
     {
       :success => 'alert-success',
@@ -8,5 +7,11 @@ module ApplicationHelper
       :notice => 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
-=end
+
+  def form_image_select(post)
+    return image_tag post.image.url(:medium),
+                     id: 'image-preview',
+                     class: 'img-responsive' if post.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+  end
 end
